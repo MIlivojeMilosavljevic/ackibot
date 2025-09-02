b1584f930584a39a32b5d3b5cd40616729d70dfd
cxxx1828 <cxxx1828@gmail.com>
2025-09-02 18:28:52 +0200

    Update ackibot_utils files

--- src/ackibot_utils/ackibot_utils/usb_mapper.py
+++ src/ackibot_utils/ackibot_utils/usb_mapper.py
@@ -1,63 +1,77 @@
+from pyudev import Context, Device  # import modula za pristupanje uređajima preko udev sistema na Linux-u
+import glob  # modul za pronalaženje fajlova po šablonima (wildcards)
 
-from pyudev import Context, Device
-import glob
+# Kod je ROS/embedded helper za automatsko prepoznavanje USB uređaja na Linux-u (npr. Raspberry Pi).
+# Umesto da ručno kucaš /dev/ttyUSB0 ili /dev/ttyACM0, ovaj kod detektuje:
+# Arduino uređaje
+# LIDAR
+# IMU
+# Debug UART
+# i automatski ih mapira po klasama.
 
+
+# Mapa koja povezuje VID:PID USB uređaja sa klasama uređaja
 vid_pid__2_class__map = {
-	'1a86:7523' : ['Arduino'], # QinHeng Electronics CH340 serial converter
-	'0403:6015' : ['Arduino'], # Future Technology Devices International, Ltd Bridge(I2C/SPI/UART/FIFO)
-	'10c4:ea60' : ['LIDAR'], # Silicon Labs CP210x UART Bridge
-	'067b:2303' : ['Debug UART', 'IMU'], # Prolific Technology, Inc. PL2303 Serial Port / Mobile Phone Data Cable
-	'0403:6001' : ['Debug UART', 'IMU'], # Future Technology Devices International, Ltd FT232 Serial (UART) IC
+    '1a86:7523' : ['Arduino'],  # QinHeng CH340 USB-Serial konverter, klasifikovan kao Arduino
+    '0403:6015' : ['Arduino'],  # FTDI USB-Bridge, klasifikovan kao Arduino
+    '10c4:ea60' : ['LIDAR'],    # Silicon Labs CP210x UART Bridge, klasifikovan kao LIDAR
+    '067b:2303' : ['Debug UART', 'IMU'],  # Prolific PL2303, može biti Debug UART ili IMU
+    '0403:6001' : ['Debug UART', 'IMU'],  # FT232 UART, može biti Debug UART ili IMU
 }
 
 class USB_Mapper:
+    """Klasa koja mapira povezane USB uređaje po klasama (Arduino, LIDAR, IMU, ...)"""
 
-	def __init__(self):
-		self.table = {}
+    def __init__(self):
+        self.table = {}  # rečnik u kojem će ključevi biti klase uređaja, a vrednosti liste device fajlova
 
-		patterns = [
-			'/dev/ttyUSB*',
-			'/dev/ttyACM*',
-		]
-		dev_files = sum(
-			[glob.glob(p) for p in patterns],
-			[]
-		)
+        # Šabloni za pretragu USB uređaja
+        patterns = [
+            '/dev/ttyUSB*',  # standardni USB-Serial uređaji
+            '/dev/ttyACM*',  # Arduino i slični USB uređaji koji koriste ACM
+        ]
+        # pronalazi sve fajlove koji odgovaraju šablonima i spaja ih u jednu listu
+        dev_files = sum(
+            [glob.glob(p) for p in patterns],
+            []
+        )
 
-		context = Context()
+        context = Context()  # kreira udev kontekst za pristup uređajima
 
-		for dev_file in dev_files:
-			dev = Device.from_device_file(context, dev_file)
+        for dev_file in dev_files:  # prolazi kroz svaki pronađeni uređaj
+            dev = Device.from_device_file(context, dev_file)  # dobija udev Device objekat
 
-			if False:
-				print(dev_file)
-				for k, v in dev.items():
-					print(k, ' = ', v)
-				print()
+            if False:  # debug opcija, trenutno ugašena
+                print(dev_file)
+                for k, v in dev.items():
+                    print(k, ' = ', v)
+                print()
 
-			#TODO Figure out port from this dev.get('ID_PATH')  =  pci-0000:00:14.0-usb-0:4.4:1.0
-			vid = dev.get('ID_VENDOR_ID') #TODO ID_USB_VENDOR_ID
-			pid = dev.get('ID_MODEL_ID')
-			vid_pid = f'{vid}:{pid}'
+            # TODO: moguće je dodatno koristiti dev.get('ID_PATH') da preciznije odredimo port
+            vid = dev.get('ID_VENDOR_ID')  # Vendor ID USB uređaja
+            pid = dev.get('ID_MODEL_ID')   # Product ID USB uređaja
+            vid_pid = f'{vid}:{pid}'       # spaja VID i PID u string
 
-			if vid_pid in vid_pid__2_class__map:
-				list_of_classes = vid_pid__2_class__map[vid_pid]
-				for c in list_of_classes:
-					if not c in self.table:
-						self.table[c] = []
-					self.table[c].append(dev_file)
-			else:
-				raise RuntimeError(
-					f'There is no {vid_pid} in vid_pid__2_class__map!'
-				)
-	def get_exactly_1_dev_of_class(self, class_name):
-		c = self.table[class_name]
-		if len(c) != 1:
-			if len(c) == 0:
-				raise RuntimeError(f'{class_name} not connected!')
-			else:
-				raise RuntimeError(f'More than 1 {class_name} connected!')
-		else:
-			return c[0]
-			
-		
+            # proverava da li je uređaj u definisanoj mapi
+            if vid_pid in vid_pid__2_class__map:
+                list_of_classes = vid_pid__2_class__map[vid_pid]  # dobija sve klase za ovaj VID:PID
+                for c in list_of_classes:
+                    if not c in self.table:  # ako klasa još nije u tabeli, dodaje je
+                        self.table[c] = []
+                    self.table[c].append(dev_file)  # dodaje device fajl u listu klase
+            else:
+                # ako uređaj nije definisan u mapi, baci grešku
+                raise RuntimeError(
+                    f'There is no {vid_pid} in vid_pid__2_class__map!'
+                )
+
+    def get_exactly_1_dev_of_class(self, class_name):
+        """Vraća tačno jedan uređaj iz date klase, ili baci grešku ako ih nema ili ih ima više"""
+        c = self.table[class_name]  # uzima listu uređaja za klasu
+        if len(c) != 1:
+            if len(c) == 0:
+                raise RuntimeError(f'{class_name} not connected!')  # nema uređaja
+            else:
+                raise RuntimeError(f'More than 1 {class_name} connected!')  # više uređaja
+        else:
+            return c[0]  # vraća jedini uređaj
